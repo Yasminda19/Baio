23e99647e546024eba20b5124cbbea5db95ad1e9
Yasminda19 <yasminsalamah@gmail.com>
2023-07-07 19:21:46 +0700

    feat(fe): proxy docs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,23 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
     proxy: {
       "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        cookieDomainRewrite: {
+          "http://localhost:5000": "http://localhost:51723",
+        },
+      },
+      "/docs": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        cookieDomainRewrite: {
+          "http://localhost:5000": "http://localhost:51723",
+        },
+      },
+      "/openapi.json": {
         target: "http://localhost:5000",
         changeOrigin: true,
         cookieDomainRewrite: {
