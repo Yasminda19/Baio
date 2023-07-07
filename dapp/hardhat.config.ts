253e71d83d53d2212cde635270ac93be1f157d57
Yasminda19 <yasminsalamah@gmail.com>
2023-07-07 19:22:16 +0700

    fart(dapp): change localstack port

--- dapp/hardhat.config.ts
+++ dapp/hardhat.config.ts
@@ -5,7 +5,7 @@ const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
     localstack: {
-      url: "http://0.0.0.0:7545",
+      url: "http://0.0.0.0:8545",
       accounts: [
         "0x6d1d883be88db72bfb33b6a32b5421a47b0e7fe26304938dbe1688ac279356b5",
         "0xe390ba782cca7cf9f74a12e4f8360e2c7e02ef842739ffff5ae9628ff3b58c0a",
