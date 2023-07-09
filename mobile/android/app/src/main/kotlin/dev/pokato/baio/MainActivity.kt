89df3966c3921ef7a7a01001747846b03f13b9cc
Yasminda19 <yasminsalamah@gmail.com>
2023-07-09 08:54:17 +0700

    refactor: deprecate old m app

--- mobile/android/app/src/main/kotlin/dev/pokato/baio/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.pokato.baio
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- mobile/lib/features/authentication/domain/app_user.dart
+++
@@ -1,7 +0,0 @@
-/// Simple class representing the user UID and email.
-class AppUser {
-  const AppUser({
-    required this.uid,
-  });
-  final String uid;
-}
